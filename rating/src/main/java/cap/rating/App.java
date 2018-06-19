96ded5285ba881b5bb47bcb7179bb2c2935a93a0
benjose <benjose@DIN76000251.corp.capgemini.com>
2018-06-19 12:36:38 +0530

    commit

---
+++ rating/src/main/java/cap/rating/App.java
@@ -0,0 +1,14 @@
+package cap.rating;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //comment
+    }
+}
